5b3133727aaff41950578efe9e4519ffb2e715e8
tangyh <tangyihang@ahakid.com>
2024-09-01 10:57:03 +0800

    添加消息通知接口

--- src/main/java/com/tencent/wxcloudrun/controller/MessageController.java
+++ src/main/java/com/tencent/wxcloudrun/controller/MessageController.java
@@ -1,8 +1,19 @@
 package com.tencent.wxcloudrun.controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @author: tangyh
  * @date: 2024/9/1
- * @description: 
- */public class MessageController {
+ * @description:
+ */
+@RestController
+public class MessageController {
+
+
+    @RequestMapping("/message")
+    public void message(@RequestBody String message){
+        System.out.println(message);
+    }
 }
